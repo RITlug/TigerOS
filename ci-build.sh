ccbd1fe171e2a13d85bdf8f4da685f9e0e921c5a
axk4545 <abkahrs@gmail.com>
2017-04-20 00:53:46 -0400

    update commands

--- ci-build.sh
+++ ci-build.sh
@@ -11,9 +11,9 @@ set -eu
 rm -rf tigeros.ks
 wget -O tigeros.ks https://raw.githubusercontent.com/RITlug/TigerOS/master/tigeros.ks
 livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever 25 --title TigerOS-live --macboot
+cp -f /var/lmc/TigerOS.iso /srv/isos/
 rm -rf /var/lmc/
-cp -f TigerOS.iso /srv/isos/
 cd /srv/isos
 sha512sum *.iso > CHECKSUM512
-
-
+cd /home/build
+rm -rf anaconda/ *.log
