2fd49d7a4c86e79a0c41662f63700774d6d78c74
axk4545 <abkahrs@gmail.com>
2017-11-02 00:14:13 -0400

    fix path in accordnace with directory choice changes

--- build-scripts/ci-build-mock.sh
+++ build-scripts/ci-build-mock.sh
@@ -20,13 +20,13 @@ mock --old-chroot -r fedora-26-x86_64 --copyin kickstarts/tigeros.ks ./tigeros.k
 mock --old-chroot -r fedora-26-x86_64 --install lorax-lmc-novirt git vim-minimal pykickstart
 mock --old-chroot -r fedora-26-x86_64 --install https://tigeros.ritlug.com/packages/x86_64/anaconda-installclass-tigeros-26-1.fc26.x86_64.rpm
 mock --old-chroot -r fedora-26-x86_64 --chroot "livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever 26 --title TigerOS-live --macboot"
-rm -rf /srv/isos/TigerOS-$(date +%Y%m%d).iso
-mock --old-chroot -r fedora-26-x86_64 --copyout /var/lmc/TigerOS.iso /srv/isos/TigerOS-$(date +%Y%m%d).iso 
+rm -rf /usr/share/nginx/html/isos/TigerOS-$(date +%Y%m%d).iso
+mock --old-chroot -r fedora-26-x86_64 --copyout /var/lmc/TigerOS.iso /usr/share/nginx/html/isos/TigerOS-$(date +%Y%m%d).iso 
 rm -rf /var/lib/mock/
-cd /srv/isos
+cd /usr/share/nginx/html/isos
 rm -rf CHECKSUM512-$(date +%Y%m%d)
 sha512sum TigerOS-$(date +%Y%m%d).iso > CHECKSUM512-$(date +%Y%m%d) 
-chown -R nginx:nginx /srv
-chmod 755 /srv/isos/*.iso
+chown -R nginx:nginx /usr/share/nginx/html/
+chmod 755 /usr/share/nginx/html/isos/*.iso
 echo "Build finished"
 
