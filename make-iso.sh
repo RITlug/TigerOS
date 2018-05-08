a8691aeb3c564f0db76010767651af3274cbd1bc
CTMartin <ct-martin@users.noreply.github.com>
2018-05-08 13:38:36 -0400

    Point make-iso.sh to the proper kickstart location

--- make-iso.sh
+++ make-iso.sh
@@ -23,6 +23,6 @@ dnf install -y lorax-lmc-novirt git vim-minimal pykickstart
 dnf install -y https://mirrors.ritlug.com/TigerOS/$vers/packages/x86_64/anaconda-installclass-tigeros-$vers-1.fc$vers.x86_64.rpm
 echo "${green}Beginning build process${reset}"
 setenforce 0
-livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever $vers --title TigerOS-live --macboot
+livemedia-creator --ks kickstarts/tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever $vers --title TigerOS-live --macboot
 echo "${green}ISO saved to $(pwd)/TigerOS.iso${reset}"
 setenforce 1
